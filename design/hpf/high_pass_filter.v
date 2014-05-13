b4b1663a6bad5e15062254c7f239136ddfd6e308
aruls <aruls.dsp@gmail.com>
2014-05-13 10:51:32 -0700

    Restructures the encoder block

--- design/hpf/high_pass_filter.v
+++ design/hpf/high_pass_filter.v
@@ -21,137 +21,73 @@
 module high_pass_filter(
     input clk,
     input ce,
+    input rst_n,
     input [15:0] audio_in,
     output reg [15:0] pre_proc_audio
     );
 
-   // Declaration of locally used signals
-   reg [31:0] 	      y0;
-   reg [31:0] 	      y1;
-   reg [31:0] 	      y2;
+  // Declaration of locally used signals
+  reg [31:0] 	      y0;
+  reg [31:0] 	      y1;
+  reg [31:0] 	      y2;
 
-   reg [15:0] 	      x1;
-   reg [15:0] 	      x2;
-      
-   reg [15:0] 	      a [2:0];
-   reg [15:0] 	      b [2:0];
+  reg [15:0] 	      x1;
+  reg [15:0] 	      x2;
+     
+  reg [15:0] 	      a [2:0];
+  reg [15:0] 	      b [2:0];
 
-//`include "fixed_point_arith.v"
-   /* This module is to define the basic arithmetic function used for 
-    fixed point arithmetic of the G729 encoder*/
+`include "fixed_point_arith.v"
 
-   /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    Basic Operations sections
-    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+  // Initialize the coefficents for the high pass filter 
+  initial 
+    begin
+     a[0] = -16'd3733;
+     a[1] = +16'd7807;
+     a[2] = +16'd4096;
 
-   /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    This function multiplies two 16 bit variables and returns a value
-    that is 32 bit of length 
-    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-   function [31:0] L_mult;
-      input [15:0]    var1,var2;
-      begin
-	 L_mult = var1 * var2;
-      end
-   endfunction // L_mult
+     b[0] = +16'd1899;
+     b[1] = -16'd3798;
+     b[2] = +16'd1899;
 
-   /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    This function multiplies two 16 bit variables and returns a value
-    that is 16 bit of length
-    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
-   function [15:0] mult;
-      input [15:0] var1,var2;
-      reg [31:0]   l_mult;
-      begin
-	 l_mult = L_mult(var1,var2);
-	 mult = l_mult >> 15;
-      end
-   endfunction // mult
+    end // initial begin
 
-   /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    32 bit operation section
-    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+  wire [15:0] x0,y1_hi,y1_lo,y2_hi,y2_lo;
 
-   // Fuction to add two 32 bit variables
-   function [31:0] L_add;
-      input [31:0] var1,var2;
-      begin
-	 L_add = var1 + var2;
-      end
-   endfunction // L_add
+  // The x0 is the incoming sample stream
+  assign x0 = audio_in;
+  assign y1_hi = y1[31:16];
+  assign y1_lo = y1[15:0];
+  assign y2_hi = y2[31:16];
+  assign y2_lo = y2[15:0];
+  
 
-   // Multiply var1 and var2 and then add with var3
-   function [31:0] L_mac;
-      input [31:0] var3;
-      input [15:0] var1,var2;
-      reg [31:0]   l_mult;
-      begin
-	 l_mult = L_mult(var1,var2);
-	 L_mac = L_add(l_mult,var3);
-      end
-   endfunction // L_mac
+  // Process to genreate the current y(0) from the incoming signals
+  always @(*) begin
+     y0 = Mpy_32_16(y1_hi,y1_lo,a[1]);
+     y0 = L_add(y0,Mpy_32_16(y2_hi,y2_lo,a[2]));
+     y0 = L_mac(y0,x0,b[0]);
+     y0 = L_mac(y0,x1,b[1]);
+     y0 = L_mac(y0,x2,b[2]);
+     y0 = y0 << 3;
+     pre_proc_audio = y0>>15;
+  end
 
 
-   // Multiply a 32 bit variable by 16 bit varaiable
-   function [31:0] Mpy_32_16;
-      input [15:0] hi,lo,n;
-      begin
-	 Mpy_32_16 = L_mult(hi,n);
-	 Mpy_32_16 = L_mac(Mpy_32_16,mult(lo,n),1);
-      end
-   endfunction // Mpy_32_16
-
-
-
-   // Initialize the coefficents for the high pass filter 
-   initial 
-     begin
-	a[0] = -16'd3733;
-	a[1] = +16'd7807;
-	a[2] = +16'd4096;
-
-	b[0] = +16'd1899;
-	b[1] = -16'd3798;
-	b[2] = +16'd1899;
-
-	y0 = 0;
-	y1 = 0;
-	y2 = 0;
-	x1 = 0;
-	x2 = 0;
-     end // initial begin
-
-   wire [15:0] x0,y1_hi,y1_lo,y2_hi,y2_lo;
-
-   // The x0 is the incoming sample stream
-   assign x0 = audio_in;
-   assign y1_hi = y1[31:16];
-   assign y1_lo = y1[15:0];
-   assign y2_hi = y2[31:16];
-   assign y2_lo = y2[15:0];
-   
-
-   // Process to genreate the current y(0) from the incoming signals
-   always @(*) begin
-      y0 = Mpy_32_16(y1_hi,y1_lo,a[1]);
-      y0 = L_add(y0,Mpy_32_16(y2_hi,y2_lo,a[2]));
-      y0 = L_mac(y0,x0,b[0]);
-      y0 = L_mac(y0,x1,b[1]);
-      y0 = L_mac(y0,x2,b[2]);
-      y0 = y0 << 3;
-      pre_proc_audio = y0>>15;
-   end
-
-
-   
    // Flip flops to store the existing filter values	 
-   always @(posedge clk) begin
-      if (ce) begin
+   always @(negedge rst_n or posedge clk) begin
+   if (!rst_n) begin
+     y0 = 0;
+     y1 = 0;
+     y2 = 0;
+     x1 = 0;
+     x2 = 0;
+   end else if(ce) begin
 	 y1 = y0;
 	 y2 = y1;
 	 x1 = x0;
 	 x2 = x1;
-      end 
+   end
    end 		
 
 
