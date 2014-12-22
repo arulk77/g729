aeeff2d0614a73a5481e5a20083faa04b47cbe4a
aruls <aruls.dsp@gmail.com>
2014-12-22 06:52:39 -0800

    Added sysgen space to the git

---
+++ sysgen/g729_init.m
@@ -0,0 +1,9 @@
+% This routine has the variable initialization for the
+% g729 encoder
+fixed_point = 16;
+fixed_point_frac = 10;
+
+fpga_up_rate = 20;
+fpga_dwn_rate = fpga_up_rate;
+
+fifo_stage1_thres = 240;
